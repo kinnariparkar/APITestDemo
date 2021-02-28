d6b8b2729dccc48059c3972356650f3be1cf51fb
kinnariparkar <47275869+kinnariparkar@users.noreply.github.com>
2021-02-28 15:58:50 +0100

    Develop (#2)
    Updated Readme and Integration Test added

---
+++ src/main/java/models/TestData.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class TestData {
+	public List<String> usernameList = new ArrayList<String>();
+
+	public List<String> getUsernameList() {
+		return usernameList;
+	}
+
+	public void setUsernameList(List<String> usernameList) {
+		this.usernameList = usernameList;
+	}
+
+
+
+}
+
+
---
+++ src/main/java/models/User.java
@@ -0,0 +1,54 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+
+	public Integer id;
+	public String name;
+	public String username;
+	public String email;
+	public Object address;
+	public String phone;
+	public String website;
+	public Object company;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", name=" + name + ", username=" + username + ", email=" + email + "]";
+	}
+
+}
